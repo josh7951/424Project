b9aaa1b56d2e0998688f24deda6502f7bc7d0122
Josh Mejia <jmejia7951@gmail.com>
2020-10-21 12:23:18 -0700

    Initial Commit/ Frameworks for login system with user verification

---
+++ resources/views/mail/signup-email.blade.php
@@ -0,0 +1,11 @@
+Hello {{$email_data['name']}}!
+<br>
+Welcome to Miraj Security!
+<br>
+Please click on the link below to verify your email and activate your account!
+<br><br>
+<a href="http://localhost/verify?code={{$email_data['verification_code']}}">Click Here!</a>
+<br><br>
+Thank you!
+<br>
+- Dev Team
