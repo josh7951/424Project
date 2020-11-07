73bdc26b0d65f9f0b3800a84c2a1fb325de60f4d
Joshua Mejia <jmejia7951@gmail.com>
2020-11-07 13:27:33 -0800

    Adding real link for live site

--- resources/views/mail/signup-email.blade.php
+++ resources/views/mail/signup-email.blade.php
@@ -4,7 +4,8 @@ Welcome to Miraj Security!
 <br>
 Please click on the link below to verify your email and activate your account!
 <br><br>
-<a href="http://localhost/verify?code={{$email_data['verification_code']}}">Click Here!</a>
+<!-- <a href="http://localhost/verify?code={{$email_data['verification_code']}}">Click Here!</a> For Testing-->
+<a href="http://54.183.98.69/verify?code={{$email_data['verification_code']}}">Click Here!</a>
 <br><br>
 Thank you!
 <br>
