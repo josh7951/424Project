46bb7dde91abedad054f60f74d960fee9714da81
Joshua Mejia <jmejia7951@gmail.com>
2020-11-14 01:58:58 -0800

    adding final url for email singup

--- resources/views/mail/signup-email.blade.php
+++ resources/views/mail/signup-email.blade.php
@@ -4,7 +4,7 @@ Welcome to Miraj Security!
 <br>
 Please click on the link below to verify your email and activate your account!
 <br><br>
-<a href="http://54.176.159.34/verify?code={{$email_data['verification_code']}}">Click Here!</a>
+<a href="https://www.miraj-security.net/verify?code={{$email_data['verification_code']}}">Click Here!</a>
 <br><br>
 Thank you!
 <br>
