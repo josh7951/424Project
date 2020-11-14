d3a6e025afbe8e48a93951faa0e6e20b0c117c8c
Joshua Mejia <jmejia7951@gmail.com>
2020-11-14 00:09:39 -0800

    Changed email url again

--- resources/views/mail/signup-email.blade.php
+++ resources/views/mail/signup-email.blade.php
@@ -4,11 +4,11 @@ Welcome to Miraj Security!
 <br>
 Please click on the link below to verify your email and activate your account!
 <br><br>
-<a href="http://www.mirajsecurity.xyz/verify?code={{$email_data['verification_code']}}">Click Here!</a>
+<a href="http://http://54.176.159.34//verify?code={{$email_data['verification_code']}}">Click Here!</a>
 <br><br>
 Thank you!
 <br>
 - Dev Team
+<!-- <br>
 <br>
-<br>
-If the above link does not work, please use <a href="http://13.52.168.17/verify?code={{$email_data['verification_code']}}">this link instead</a>.
+If the above link does not work, please use <a href="http://13.52.168.17/verify?code={{$email_data['verification_code']}}">this link instead</a>. -->
